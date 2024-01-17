18b3349b88c419064c80a820dd18b017228c559b
Andrew Parry <2393265p@student.gla.ac.uk>
2024-01-17 14:15:24 +0000

    add pass for max iters

--- partitionrank/eval/lit5.py
+++ partitionrank/eval/lit5.py
@@ -6,11 +6,11 @@ from fire import Fire
 from os.path import join
 from json import dump
 
-def score_lit_t5(dataset : str, topics_or_res : str, output_path : str, checkpoint : str, window_size : int = 20, stride : int = 10, mode : str = 'sliding', buffer : int = 20, **kwargs):
+def score_lit_t5(dataset : str, topics_or_res : str, output_path : str, checkpoint : str, window_size : int = 20, stride : int = 10, mode : str = 'sliding', buffer : int = 20, max_iters : int = 100, **kwargs):
     topics_or_res = read_results(topics_or_res)
     out_file = join(output_path, f"lit5.{mode}.{buffer}.{window_size}.{stride}.tsv.gz")
     log_file = join(output_path, f"lit5.{mode}.{buffer}.{window_size}.{stride}.log")
-    model = LiT5(model_path=checkpoint, mode=mode, window_size=window_size, buffer=buffer, stride=stride)
+    model = LiT5(model_path=checkpoint, mode=mode, window_size=window_size, buffer=buffer, stride=stride, max_iters=max_iters)
     model = pt.text.get_text(dataset, "text") >> model
 
     res = model.transform(topics_or_res)
