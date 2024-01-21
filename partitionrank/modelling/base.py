fda6551ee25a366800d755346b0ebfca68845f81
Andrew Parry <2393265p@student.gla.ac.uk>
2024-01-21 16:50:00 +0000

    hmmm

--- partitionrank/modelling/base.py
+++ partitionrank/modelling/base.py
@@ -11,13 +11,6 @@ class LLMRanker:
             device = 'cuda' if n_gpu else 'cpu'
         self.device = device
         self._model, self._tokenizer = load_model(checkpoint, device=device, num_gpus=n_gpu)
-        
-        config = GenerationConfig.from_model_config(self._model.config)
-        config.max_new_tokens = max_new_tokens
-        config.min_length = 1
-        config.top_p = 1.0
-        config.do_sample = False
-        self.generation_config = config
 
     def parse_output(self, output : str, length : int) -> List[int]:
         output = re.sub(r'[^0-9]', ' ', output) # clean outputs (keep only digits)
@@ -30,7 +23,12 @@ class LLMRanker:
         if isinstance(text, str): text = [text]
         inputs = self._tokenizer(text)
         inputs = {k: torch.tensor(v).to(self.device) for k, v in inputs.items()}
-        output_ids = self._model.generate(**inputs, generation_config=self.generation_config)
+        gen_cfg = GenerationConfig.from_model_config(self._model.config)
+        gen_cfg.max_new_tokens = self.max_new_tokens
+        gen_cfg.min_length = 1
+        # gen_cfg.temperature = 0
+        gen_cfg.do_sample = False
+        output_ids = self._model.generate(**inputs, generation_config=gen_cfg)
         if self._model.config.is_encoder_decoder: output_ids = output_ids[0]
         else: output_ids = output_ids[0][len(inputs["input_ids"][0]):]
         outputs = self._tokenizer.decode(output_ids, skip_special_tokens=True, spaces_between_special_tokens=False)
