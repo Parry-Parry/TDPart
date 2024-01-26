85a58759b8f5b332b7669212b91800ad9ae48769
Andrew Parry <2393265p@student.gla.ac.uk>
2024-01-26 11:23:00 +0000

    fixing eval

--- partitionrank/eval/synthetic/evaluate.py
+++ partitionrank/eval/synthetic/evaluate.py
@@ -32,15 +32,13 @@ def evaluate(in_path : str, out_path : str, model : Any):
     }
 
     
-
+    progress = tqdm(total=10*3*(len(RATIOS[5]) + len(RATIOS[10]) + len(RATIOS[20])))
     for window_len in [5, 10, 20]:
         _model = LOAD_FUNCS[model](dataset=pt.get_dataset('irds:msmarco-passage'), mode='single', window_size=window_len, cutoff=window_len-1)
-        progress = tqdm(total=10*3*3*len(RATIOS[window_len]))
         for i in range(10):
             for order in range(3):
                 _order = Order(order)
                 for ratio in RATIOS[window_len]:
-                    out_frame = output.copy()
                     _ratio = str(ratio).replace('.', '_')
                     input_name = f"{_order.name}.{_ratio}.{window_len}.{i}.tsv.gz"
                     sample = pd.read_csv(join(in_path, input_name), sep='\t', dtype={'docno': str, 'qid': str})
@@ -49,19 +47,19 @@ def evaluate(in_path : str, out_path : str, model : Any):
                     rez = _model.transform(sample)
                     metrics = eval.calc_aggregate(rez.rename(columns={'docno': 'doc_id', 'qid': 'query_id'}))
                     metrics = {str(k) : v for k, v in metrics.items()}
-                    out_frame['iter'].append(i)
-                    out_frame['ratio'].append(ratio)
-                    out_frame['window_len'].append(window_len)
-                    out_frame['order'].append(_order.name)
-                    out_frame['before_nDCG@10'].append(old_metrics['nDCG@10'])
-                    out_frame['before_nDCG@5'].append(old_metrics['nDCG@5'])
-                    out_frame['before_nDCG@1'].append(old_metrics['nDCG@1'])
-                    out_frame['after_nDCG@10'].append(metrics['nDCG@10'])
-                    out_frame['after_nDCG@5'].append(metrics['nDCG@5'])
-                    out_frame['after_nDCG@1'].append(metrics['nDCG@1'])
+                    output['iter'].append(i)
+                    output['ratio'].append(ratio)
+                    output['window_len'].append(window_len)
+                    output['order'].append(_order.name)
+                    output['before_nDCG@10'].append(old_metrics['nDCG@10'])
+                    output['before_nDCG@5'].append(old_metrics['nDCG@5'])
+                    output['before_nDCG@1'].append(old_metrics['nDCG@1'])
+                    output['after_nDCG@10'].append(metrics['nDCG@10'])
+                    output['after_nDCG@5'].append(metrics['nDCG@5'])
+                    output['after_nDCG@1'].append(metrics['nDCG@1'])
                     progress.update(1)
     
-    out_name = f"{model}.{i}.tsv.gz"
+    out_name = f"{model}.tsv.gz"
     pd.DataFrame(output).to_csv(join(out_path, out_name), sep='\t', index=False)
 
 if __name__ == '__main__':
