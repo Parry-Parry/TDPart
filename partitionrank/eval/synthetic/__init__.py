0072bf287840b53ccf33c3e7d3c6df31486f7211
Andrew Parry <a.parry.1@research.gla.ac.uk>
2024-04-30 16:54:52 +0100

    need to fix but its getting there

--- partitionrank/eval/synthetic/__init__.py
+++ partitionrank/eval/synthetic/__init__.py
@@ -1,3 +1,4 @@
+import copy
 from enum import Enum
 import pandas as pd
 
@@ -26,3 +27,25 @@ def get_sample(qrels, qid, num_items : int = 20, order = Order.RANDOM, ratio : i
     qrels = qrels.rename(columns={'doc_id': 'docno', 'query_id': 'qid'}).reset_index(drop=True)
     qrels['score'] = [i for i in range(num_items)]
     return qrels
+
+class Generator(object):
+    def __init__(self, qrels, num_items : int = 20, cutoff : int = 2, ratios : float = [0.2, 0.4, 0.6, 0.8]):
+        self.qrels = qrels
+        self.qids = self.qrels['query_id'].unique()
+        self.num_items = num_items
+        self.cutoff = cutoff
+        self.ratios = ratios
+
+        default = {qid : None for qid in self.qrels['query_id'].unique()}
+        self.current, self.rel, self.nrel = copy(default), copy(default), copy(default)
+        self.new_sample()
+    
+    def new_sample(self):
+        for qid in self.qids:
+            self.rel[qid] = self.qrels[(self.qrels['query_id'] == qid) & (self.qrels['relevance'] >= self.cutoff)].copy().sample(n=self.num_items, replace=False)
+            self.nrel[qid] = self.qrels[(self.qrels['query_id'] == qid) & (self.qrels['relevance'] < self.cutoff)].copy().sample(n=self.num_items, replace=False)
+    
+    def get_samples(self, qid):
+        for ratio in self.ratios:
+            self.current[qid] = pd.concat([self.rel[qid].sample(n=int(ratio*self.num_items), replace=False), self.nrel[qid].sample(n=int((1-ratio)*self.num_items), replace=False)]).reset_index(drop=True)
+            yield self.current[qid]
